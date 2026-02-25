1e30e7a1ae410399e3d038274fda49099be2c562
Patrick <patrick_schauer@t-online.de>
2026-02-25 15:31:02 +0100

    feat: example data from Supabase available in board component

--- src/app/pages/board/board.ts
+++ src/app/pages/board/board.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, computed, inject, effect } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TasksDb, Task } from '../../core/db/tasks.db';
 
 @Component({
   selector: 'app-board',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './board.html',
   styleUrl: './board.scss',
 })
-export class Board {}
+export class Board implements OnInit, OnDestroy {
+  private tasksDb = inject(TasksDb);
+
+  // All tasks from DB
+  tasks = this.tasksDb.tasks;
+
+  // Filtered by status for each kanban column
+  todoTasks = computed(() => this.tasks().filter((t) => t.status === 'todo'));
+  inProgressTasks = computed(() => this.tasks().filter((t) => t.status === 'in-progress'));
+  awaitFeedbackTasks = computed(() => this.tasks().filter((t) => t.status === 'await-feedback'));
+  doneTasks = computed(() => this.tasks().filter((t) => t.status === 'done'));
+
+  async ngOnInit() {
+    await this.tasksDb.getTasks();
+    this.tasksDb.subscribeToTaskChanges();
+  }
+
+  ngOnDestroy() {
+    this.tasksDb.unsubscribeFromTaskChanges();
+    this.tasksDb.subscribeToTaskChanges();
+  }
+}
