77c05343f11b59e47e35ed72573dd3ab2c0e2a06
LevinKrischer <levin@levinkrischer.de>
2026-04-10 11:31:54 +0200

    refactor/ui: html and scss files of privacy policy and legal-notice

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { BackButton } from '../../shared/ui/forms/back-button/back-button';
+import { Main } from '../../shared/ui/main/main';
 
 @Component({
   selector: 'app-legal-notice',
   standalone: true,
-  imports: [BackButton],
+  imports: [BackButton, Main],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
--- src/app/pages/privacy-policy/privacy-policy.ts
+++ src/app/pages/privacy-policy/privacy-policy.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { BackButton } from '../../shared/ui/forms/back-button/back-button';
+import { Main } from '../../shared/ui/main/main';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [BackButton],
+  imports: [BackButton, Main],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
 
-export class PrivacyPolicy {
-
-}
+export class PrivacyPolicy {}
